632d120f60474a637baad578a4185f9f25214afd
ikhsan <ikhsansudiramubaroq2910@gmail.com>
2026-04-13 21:53:56 +0700

    Add PostgreSQL database service to docker-compose

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+""" 
+Calculator Functoin
+"""
+
+def add(x,y):
+    """Add x dan y and return results."""
+    return x + y
+
+def subtract(x ,y):
+    """subtract x from y and return result."""
+    return y - x
